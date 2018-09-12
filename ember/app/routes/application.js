6db99053af76c6f73d22fc6dedb0b84d5f34ceaa
Josh Hemen <melombuki@gmail.com>
2018-09-11 22:19:07 -0500

    Move add component to podcasts#index
    - Prevents the subscription input from showing up everywhere

--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -1,22 +1,4 @@
 import AuthenticatedRoute from './authenticated';
 
 export default AuthenticatedRoute.extend({
-  actions: {
-    subscribe(url) {
-      console.log(url);
-      return fetch('http://localhost:3000/api/podcasts', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          "Authorization": this.authService.getAccessToken(),
-        },
-        body: JSON.stringify({ url: url })
-      })
-        .then(() => {
-          this.set('url', null);
-          this.refresh();
-        });
-    }
-  }
 });
--- ember/app/routes/podcasts/index.js
+++ ember/app/routes/podcasts/index.js
@@ -9,6 +9,25 @@ export default AuthenticatedRoute.extend({
     });
   },
 
+  actions: {
+    subscribe(url) {
+      console.log(url);
+      return fetch('http://localhost:3000/api/podcasts', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          "Authorization": this.authService.getAccessToken(),
+        },
+        body: JSON.stringify({ url: url })
+      })
+        .then(() => {
+          this.set('url', null);
+          this.refresh();
+        });
+    }
+  },
+
   queryParams: {
     page: {
       refreshModel: true,
