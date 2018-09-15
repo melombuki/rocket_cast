20483c2e4ae3e92d20ebbb914a364d885010ea95
Josh Hemen <melombuki@gmail.com>
2018-09-15 11:43:09 -0500

    Add ability to delete a subscription

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   
   namespace :api do
     post :authenticate, to: 'authentication#authenticate'
+
     patch :podcasts, to: 'podcasts#update'
+    delete 'podcasts/:id', to: 'podcasts#destroy'
+
     jsonapi_resources :podcasts, except: [:new, :edit, :update] do
       jsonapi_relationships
     end
